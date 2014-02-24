7863e2faed89a8db3ce7f1d55212c77907452875
awayings <awayings@gmail.com>
2014-02-24 14:37:44 +0000

    add counter for playing time.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    if session[:counter].nil?
+      @counter = 1
+    else
+      @counter = session[:counter]
+      @counter +=1
+    end
+      session[:counter] = @counter
+
   end
 end
